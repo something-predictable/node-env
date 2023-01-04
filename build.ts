7bef8d8906c038ec0487b3beda3181fe928050d7
k15r <git@riddance.k15r.xyz>
2023-01-04 23:59:27 +0100

    Remove restriction on source folder structure.

--- build.ts
+++ build.ts
@@ -1,23 +1,10 @@
-import { readdir } from 'node:fs/promises'
-import { join } from 'node:path'
-import { getSource, load } from './lib/changes.js'
+import { load } from './lib/changes.js'
 import { compile } from './lib/compiler.js'
 import { Reporter } from './lib/reporter.js'
 
 export async function build(reporter: Reporter, path: string) {
     const changes = await load(path)
     await changes.preCompile(reporter, path)
-    const { sourceFiles, outputFiles } = await compile(reporter, path, await findFiles(path))
+    const { sourceFiles, outputFiles } = await compile(reporter, path)
     return await changes.postCompile(reporter, path, sourceFiles, Promise.resolve(outputFiles))
-}
-
-async function findFiles(path: string) {
-    const [root, test, bin] = await Promise.all([
-        readdir(path),
-        readdir(join(path, 'test')),
-        readdir(join(path, 'bin')),
-    ])
-    return getSource(
-        [root, test.map(p => join('test', p)), bin.map(p => join('bin', p))].flatMap(d => d),
-    )
 }
--- lib/compiler.ts
+++ lib/compiler.ts
@@ -3,18 +3,22 @@ import { relative, resolve } from 'node:path'
 import ts from 'typescript'
 import { Reporter } from './reporter.js'
 
-export async function compile(reporter: Reporter, path: string, inputFiles: string[]) {
+export async function compile(reporter: Reporter, path: string) {
     const tsconfig = JSON.parse(
         (await readFile(resolve(path, 'tsconfig.json'))).toString('utf-8'),
     ) as {
         compilerOptions: unknown
         include: string[]
+        exclude: string[]
     }
     const options = ts.convertCompilerOptionsFromJson(tsconfig.compilerOptions, path)
     if (options.errors.length !== 0) {
         options.errors.forEach(reportDiagnostic(reporter))
         return { sourceFiles: [] }
     }
+    const inputFiles = ts.sys
+        .readDirectory(path, undefined, tsconfig.exclude, tsconfig.include)
+        .filter(f => !f.endsWith('.d.ts'))
 
     const program = ts.createProgram(
         inputFiles.map(f => resolve(path, f)),
