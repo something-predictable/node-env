be11e801480473684383280ff2e324bc768aaca8
k15r <git@riddance.k15r.xyz>
2024-02-25 16:21:54 +0100

    Log relative paths
    to reduce console clutter.

--- bin/lib/consoleReporter.ts
+++ bin/lib/consoleReporter.ts
@@ -1,9 +1,18 @@
+import { relative } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const cwd = process.cwd()
+
 export const consoleReporter = {
     status: (message: string) => console.log(message),
     error: (message: string, file?: string, line?: number, column?: number) => {
         let context = ''
         if (file) {
-            context += file
+            if (file.startsWith('file://')) {
+                context += relative(cwd, fileURLToPath(file))
+            } else {
+                context += relative(cwd, file)
+            }
             if (line) {
                 context += ':' + line.toString()
                 if (column) {
